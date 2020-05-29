a0a5d69694e81dd032b9ae4187fcc614eed64219
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-05-29 11:29:43 +0100

    Refactored to be more functional

--- main.js
+++ main.js
@@ -7,37 +7,41 @@ var display = document.querySelector(".timer");
 var tickAudio = new Audio("beep-04.mp3");
 var countdown;
 function timer(seconds) {
+    var timeNowInSeconds = Date.now();
+    var then = timeNowInSeconds + seconds * 1000;
     clearInterval(countdown);
-    var now = Date.now();
-    var then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    startCountdown(then);
+}
+function startCountdown(then) {
     countdown = setInterval(function () {
-        var secondLeft = Math.round((then - Date.now()) / 1000);
-        if (secondLeft < 0) {
+        var secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft < 0) {
             clearInterval(countdown);
             tickAudio.play();
             return;
         }
-        displayTimeLeft(secondLeft);
+        else
+            displayTimeLeft(secondsLeft);
     }, 1000);
 }
 function displayTimeLeft(seconds) {
     var minutes = Math.floor(seconds / 60);
-    var remainderSeconds = seconds % 60;
-    if (remainderSeconds < 10) {
-        display.textContent = minutes + " : 0" + remainderSeconds;
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds < 10) {
+        display.textContent = minutes + " : 0" + remainingSeconds;
     }
-    else if (remainderSeconds > 10) {
-        display.textContent = minutes + " : " + remainderSeconds;
-    }
+    else
+        remainingSeconds > 10;
+    display.textContent = minutes + " : " + remainingSeconds;
     document.title = display.textContent;
 }
-workButton.addEventListener("click", function (e) {
+workButton.addEventListener("click", function () {
     timer(1500);
 });
-breakButton.addEventListener("click", function (e) {
+breakButton.addEventListener("click", function () {
     timer(300);
 });
-longBreakbutton.addEventListener("click", function (e) {
+longBreakbutton.addEventListener("click", function () {
     timer(1800);
 });
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 export {};
+
 const workButton = document.getElementById("work");
 const breakButton = document.getElementById("break");
 const longBreakbutton = document.getElementById("long-break");
@@ -8,30 +9,32 @@ const tickAudio = new Audio("beep-04.mp3");
 let countdown: number;
 
 function timer(seconds: number) {
+  const timeNowInSeconds: number = Date.now();
+  const then: number = timeNowInSeconds + seconds * 1000;
   clearInterval(countdown);
-  const now = Date.now();
-  const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  startCountdown(then);
+}
 
+function startCountdown(then: number) {
   countdown = setInterval(() => {
-    const secondLeft: number = Math.round((then - Date.now()) / 1000);
-    if (secondLeft < 0) {
+    const secondsLeft: number = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
       clearInterval(countdown);
       tickAudio.play();
       return;
-    }
-    displayTimeLeft(secondLeft);
+    } else displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
 function displayTimeLeft(seconds: number) {
   const minutes: number = Math.floor(seconds / 60);
-  const remainderSeconds: number = seconds % 60;
-  if (remainderSeconds < 10) {
-    display.textContent = `${minutes} : 0${remainderSeconds}`;
-  } else if (remainderSeconds > 10) {
-    display.textContent = `${minutes} : ${remainderSeconds}`;
-  }
+  const remainingSeconds: number = seconds % 60;
+
+  if (remainingSeconds < 10) {
+    display.textContent = `${minutes} : 0${remainingSeconds}`;
+  } else remainingSeconds > 10;
+  display.textContent = `${minutes} : ${remainingSeconds}`;
   document.title = display.textContent;
 }
 
